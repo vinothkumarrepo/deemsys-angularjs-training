2e37692e407ff86db1a7539b46bcd2328421e9b4
vinothkumarrepo <vinothkumarg@deemsysinc.com>
2016-08-10 11:34:30 +0530

    Form div updated.

--- js/practice.js
+++ js/practice.js
@@ -51,3 +51,16 @@ str=[];
 }]);
 
 
+appng.controller('formEg', ['$scope', function($scope) {
+      $scope.master = {};
+
+      $scope.update = function(user) {
+        $scope.master = angular.copy(user);
+      };
+
+      $scope.reset = function() {
+        $scope.user = angular.copy($scope.master);
+      };
+
+      $scope.reset();
+    }]);
